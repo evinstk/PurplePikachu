3871467362ae07af5b060575e1828ebb28207ef9
Tanner Evins <tanner.evins@gmail.com>
2014-04-14 20:10:10 -0500

    Complete constructor, implement attach/detach

--- src/SceneNode.cpp
+++ src/SceneNode.cpp
@@ -1,12 +1,32 @@
 #include <TEMM/SceneNode.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
+#include <cassert>
 
 namespace temm
 {
 
 	SceneNode::SceneNode(Category::Type category)
 		: mDefaultCategory(category)
+		, mChildren()
+		, mParent(nullptr)
 	{
+	}
+
+	void SceneNode::attachChild(Ptr child)
+	{
+		child->mParent = this;
+		mChildren.push_back(std::move(child));
+	}
+
+	SceneNode::Ptr SceneNode::detachChild(const SceneNode& node)
+	{
+		auto found = std::find_if(mChildren.begin(), mChildren.end(), [&](Ptr& p) {return p.get() == &node; });
+		assert(found != mChildren.end());
+
+		Ptr result = std::move(*found);
+		result->mParent = nullptr;
+		mChildren.erase(found);
+		return result;
 	}
 
 	void SceneNode::draw(sf::RenderTarget& target, sf::RenderStates states) const
