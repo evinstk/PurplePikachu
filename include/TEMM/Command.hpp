73f3a8b67b5dcbd73c31838a3561518f324200b7
evinstk <tanner.evins@gmail.com>
2014-04-15 15:13:00 -0500

    Add Command
    Command stores Action function to be performed on a SceneNode matching the
    prescribed category. Use by pushing down the scene graph and allowing
    matching SceneNodes to execute.

---
+++ include/TEMM/Command.hpp
@@ -0,0 +1,33 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <TEMM/Category.hpp>
+#include <SFML/System/Time.hpp>
+#include <functional>
+#include <cassert>
+
+namespace temm
+{
+	class SceneNode;
+
+	struct Command
+	{
+		typedef std::function<void(SceneNode&, sf::Time)> Action;
+		Command();
+		Action action;
+		unsigned int category;
+	};
+
+	template <typename GameObject, typename Function>
+	Command::Action derivedAction(Function fn)
+	{
+		return[=](SceneNode& node, sf::Time dt)
+		{
+			assert(dynamic_cast<GameObject*>(&node) != nullptr);
+
+			fn(static_cast<GameObject&>(node), dt);
+		}
+	}
+}
+
+#endif
